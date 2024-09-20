e9d717b561d1ccbfd0260ffa7949cd4e093fac64
Romance0806 <romance7486@gmail.com>
2024-09-20 13:44:30 +0800

    Add files via upload

---
+++ 20221108/js/action.js
@@ -0,0 +1,26 @@
+/*----lottery----*/
+var timer;
+$(window).load(function(){
+    timer = setTimeout(function() {animationRoll(500);},0);
+});
+/*----PAGE TAB----*/
+$(function() {	
+	
+	$(".btn02 a").click(function(){
+		$("#evt").toggleClass("on");
+	});
+
+	$("#close").click(function() {
+		$('#evt').toggleClass("on");
+	});
+
+});
+
+	/*----mob_menu----*/
+$(function() {
+  $('.btn.open').click(function(){ 
+    $('.nav_mob').toggleClass('on');
+    $(this).toggleClass('close');
+  });
+	
+});
