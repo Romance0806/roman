4d22e7614a805e00655d8aa4ea1724a848a3bb38
Romance0806 <romance7486@gmail.com>
2024-10-28 15:15:24 +0800

    Add files via upload

---
+++ AA20190920/js/action.js
@@ -0,0 +1,204 @@
+﻿$(document).ready(function() {
+	$('#pay').hide();
+	
+              var owl = $('.calendar .owl-carousel');
+              owl.owlCarousel({
+                items: 1,
+                loop: false,
+                margin: 0,
+				dots: false,
+				nav: true,
+				navText: ["<img src='images/btn_prev.png'>","<img src='images/btn_next.png'>"]
+              });
+	$('#task01 a, #task02 a, #task03 a, #task04 a, #task05 a, #task18 a, #task19 a, #task20 a, #task21 a, #task22 a, #task35 a, #task36 a, #task37 a, #task38 a, #task39 a, #task52 a, #task53 a, #task54 a, #task55 a, #task56 a').click(function() {
+		$('#pay').fadeIn();
+		});
+	
+	$("#close, .reveal-modal-bg").click(function() {
+		$('#pay').fadeOut();
+	});
+	});
+	
+	
+	
+	
+	$(document).ready(function() {
+	$('#pay01').hide();
+	
+              var owl = $('.calendar .owl-carousel');
+              owl.owlCarousel({
+                items: 1,
+                loop: false,
+                margin: 0,
+				dots: false,
+				nav: true,
+				navText: ["<img src='images/btn_prev.png'>","<img src='images/btn_next.png'>"]
+              });
+	$('#task07 a, #task08 a, #task09 a, #task10 a, #task11 a, #task24 a, #task25 a, #task26 a, #task27 a, #task28 a, #task41 a, #task42 a, #task43 a, #task44 a, #task45 a, #task58 a, #task59 a, #task60 a, #task61 a, #task62 a').click(function() {
+		$('#pay01').fadeIn();
+		});
+	
+	$("#close, .reveal-modal-bg").click(function() {
+		$('#pay01').fadeOut();
+	});
+	});
+	
+	$(document).ready(function() {
+	$('#pay02').hide();
+	
+              var owl = $('.calendar .owl-carousel');
+              owl.owlCarousel({
+                items: 1,
+                loop: false,
+                margin: 0,
+				dots: false,
+				nav: true,
+				navText: ["<img src='images/btn_prev.png'>","<img src='images/btn_next.png'>"]
+              });
+	$('#task13 a, #task14 a, #task15 a, #task16 a, #task17 a, #task30 a, #task31 a, #task32 a, #task33 a, #task34 a, #task47 a, #task48 a, #task49 a, #task50 a, #task51 a, #task64 a, #task65 a, #task66 a, #task67 a, #task68 a').click(function() {
+		$('#pay02').fadeIn();
+		});
+	
+	$("#close, .reveal-modal-bg").click(function() {
+		$('#pay02').fadeOut();
+	});
+	});
+	
+	
+	$(document).ready(function() {
+	$('#pay03').hide();
+	
+              var owl = $('.calendar .owl-carousel');
+              owl.owlCarousel({
+                items: 1,
+                loop: false,
+                margin: 0,
+				dots: false,
+				nav: true,
+				navText: ["<img src='images/btn_prev.png'>","<img src='images/btn_next.png'>"]
+              });
+	$('#task69 a, #task70 a, #task71 a, #task72 a').click(function() {
+		$('#pay03').fadeIn();
+		});
+	
+	$("#close, .reveal-modal-bg").click(function() {
+		$('#pay03').fadeOut();
+	});
+	});
+	
+	$(document).ready(function() {
+	$('#pay04').hide();
+	
+              var owl = $('.calendar .owl-carousel');
+              owl.owlCarousel({
+                items: 1,
+                loop: false,
+                margin: 0,
+				dots: false,
+				nav: true,
+				navText: ["<img src='images/btn_prev.png'>","<img src='images/btn_next.png'>"]
+              });
+	$('#task06 a, #task23 a, #task40 a, #task57 a').click(function() {
+		$('#pay04').fadeIn();
+		});
+	
+	$("#close, .reveal-modal-bg").click(function() {
+		$('#pay04').fadeOut();
+	});
+	});
+	
+	$(document).ready(function() {
+	$('#pay05').hide();
+	
+              var owl = $('.calendar .owl-carousel');
+              owl.owlCarousel({
+                items: 1,
+                loop: false,
+                margin: 0,
+				dots: false,
+				nav: true,
+				navText: ["<img src='images/btn_prev.png'>","<img src='images/btn_next.png'>"]
+              });
+	$('#task12 a, #task29 a, #task46 a, #task63 a').click(function() {
+		$('#pay05').fadeIn();
+		});
+	
+	$("#close, .reveal-modal-bg").click(function() {
+		$('#pay05').fadeOut();
+	});
+	});
+	
+	$(document).ready(function() {
+	$('#pay06').hide();
+	
+              var owl = $('.calendar .owl-carousel');
+              owl.owlCarousel({
+                items: 1,
+                loop: false,
+                margin: 0,
+				dots: false,
+				nav: true,
+				navText: ["<img src='images/btn_prev.png'>","<img src='images/btn_next.png'>"]
+              });
+	$('.level a').click(function() {
+		$('#pay06').fadeIn();
+		});
+	
+	$("#close, .reveal-modal-bg").click(function() {
+		$('#pay06').fadeOut();
+	});
+	});
+	
+	$(document).ready(function() {
+	$('#pay07').hide();
+	
+              var owl = $('.calendar .owl-carousel');
+              owl.owlCarousel({
+                items: 1,
+                loop: false,
+                margin: 0,
+				dots: false,
+				nav: true,
+				navText: ["<img src='images/btn_prev.png'>","<img src='images/btn_next.png'>"]
+              });
+	$('.list a').click(function() {
+		$('#pay07').fadeIn();
+		});
+	
+	$("#close, .reveal-modal-bg").click(function() {
+		$('#pay07').fadeOut();
+	});
+	});
+	
+		$(document).ready(function() {
+	$('#pay08').hide();
+	
+              var owl = $('.calendar .owl-carousel');
+              owl.owlCarousel({
+                items: 1,
+                loop: false,
+                margin: 0,
+				dots: false,
+				nav: true,
+				navText: ["<img src='images/btn_prev.png'>","<img src='images/btn_next.png'>"]
+              });
+	$('.start a').click(function() {
+		$('#pay08').fadeIn();
+		});
+	
+	$("#close, .reveal-modal-bg, .pay_btn li a").click(function() {
+		$('#pay08').fadeOut();
+	});
+	});
+	<!--timer-->
+	$(function(){
+	$(".start_ok a").hide();
+    $(".btn_ok a").one('click', function(){
+	$('.timer').startTimer({
+    onComplete: function(element){
+      $(".start a").hide();
+	  $(".start_ok a").show();
+    }
+  }).click(function(){ location.reload() });
+  }); 
+})
---
+++ AA20190920/js/jquery.reveal.js
@@ -0,0 +1,200 @@
+
+(function($) {
+
+/*---------------------------
+ Defaults for Reveal
+----------------------------*/
+	 
+/*---------------------------
+ Listener for data-reveal-id attributes
+----------------------------*/
+
+	$('a[data-reveal-id]').live('click', function(e) {
+		e.preventDefault();
+		var modalLocation = $(this).attr('data-reveal-id');
+		$('#'+modalLocation).reveal($(this).data());
+	});
+
+/*---------------------------
+ Extend and Execute
+----------------------------*/
+
+    $.fn.reveal = function(options) {
+        
+        
+        var defaults = {  
+	    	animation: 'fadeAndPop', //fade, fadeAndPop, none
+		    animationspeed: 300, //how fast animtions are
+		    closeonbackgroundclick: true, //if you click background will modal close?
+		    dismissmodalclass: 'close-reveal-modal' //the class of a button or element that will close an open modal
+    	}; 
+    	
+        //Extend dem' options
+        var options = $.extend({}, defaults, options); 
+	
+        return this.each(function() {
+        
+/*---------------------------
+ Global Variables
+----------------------------*/
+        	var modal = $(this),
+        		topMeasure  = parseInt(modal.css('top')),
+				topOffset = modal.height() + topMeasure,
+          		locked = false,
+				modalBG = $('.reveal-modal-bg');
+
+/*---------------------------
+ Create Modal BG
+----------------------------*/
+			if(modalBG.length == 0) {
+				modalBG = $('<div class="reveal-modal-bg" />').insertAfter(modal);
+			}		    
+     
+/*---------------------------
+ Open & Close Animations
+----------------------------*/
+			//Entrance Animations
+			modal.bind('reveal:open', function () {
+			  modalBG.unbind('click.modalEvent');
+				$('.' + options.dismissmodalclass).unbind('click.modalEvent');
+				if(!locked) {
+					lockModal();
+					if(options.animation == "fadeAndPop") {
+						modal.css({'top': $(document).scrollTop()-topOffset, 'opacity' : 0, 'visibility' : 'visible'});
+						modalBG.fadeIn(options.animationspeed/2);
+						modal.delay(options.animationspeed/2).animate({
+							"top": $(document).scrollTop()+topMeasure + 'px',
+							"opacity" : 1,
+							"top" : -50
+							
+						}, options.animationspeed,unlockModal());					
+					}
+					if(options.animation == "fade") {
+						modal.css({'opacity' : 0, 'visibility' : 'visible', 'top': $(document).scrollTop()+topMeasure});
+						modalBG.fadeIn(options.animationspeed/2);
+						modal.delay(options.animationspeed/2).animate({
+							"opacity" : 1
+						}, options.animationspeed,unlockModal());					
+					} 
+					if(options.animation == "none") {
+						modal.css({'visibility' : 'visible', 'top':$(document).scrollTop()+topMeasure});
+						modalBG.css({"display":"block"});	
+						modalBG.css({"top": 40});	
+						unlockModal()				
+					}
+				}
+				modal.unbind('reveal:open');
+			}); 	
+
+			//Closing Animation
+			modal.bind('reveal:close', function () {
+			  if(!locked) {
+					lockModal();
+					if(options.animation == "fadeAndPop") {
+						modalBG.delay(options.animationspeed).fadeOut(options.animationspeed);
+						modal.animate({
+							"top":  $(document).scrollTop()-topOffset + 'px',
+							"opacity" : 0
+						}, options.animationspeed/2, function() {
+							modal.css({'top':topMeasure, 'opacity' : 1, 'visibility' : 'hidden'});
+							unlockModal();
+						});					
+					}  	
+					if(options.animation == "fade") {
+						modalBG.delay(options.animationspeed).fadeOut(options.animationspeed);
+						modal.animate({
+							"opacity" : 0
+						}, options.animationspeed, function() {
+							modal.css({'opacity' : 1, 'visibility' : 'hidden', 'top' : topMeasure});
+							unlockModal();
+						});					
+					}  	
+					if(options.animation == "none") {
+						modal.css({'visibility' : 'hidden', 'top' : topMeasure});
+						modalBG.css({'display' : 'none'});	
+					}		
+				}
+				modal.unbind('reveal:close');
+			});     
+   	
+/*---------------------------
+ Open and add Closing Listeners
+----------------------------*/
+        	//Open Modal Immediately
+    	modal.trigger('reveal:open')
+			
+			//Close Modal Listeners
+			var closeButton = $('.' + options.dismissmodalclass).bind('click.modalEvent', function () {
+			  modal.trigger('reveal:close')
+			});
+			
+			if(options.closeonbackgroundclick) {
+				modalBG.css({"cursor":"pointer"})
+				modalBG.bind('click.modalEvent', function () {
+				  modal.trigger('reveal:close')
+				});
+			}
+			$('body').keyup(function(e) {
+        		if(e.which===27){ modal.trigger('reveal:close'); } // 27 is the keycode for the Escape key
+			});
+			
+			
+/*---------------------------
+ Animations Locks
+----------------------------*/
+			function unlockModal() { 
+				locked = false;
+			}
+			function lockModal() {
+				locked = true;
+			}	
+			
+        });//each call
+    }//orbit plugin call
+})(jQuery);
+        
+/*----rainy----*/
+$(document).ready(function(){
+  window.onload = function() {
+    var canvas = document.getElementById('rainy');
+    var ctx = canvas.getContext('2d');
+
+    var W = window.innerWidth;
+    var H = window.innerHeight;
+    canvas.width = W;
+    canvas.height = H;
+    
+    var num = 200;
+    var arr = [];
+    var speed = 5;
+    
+    for (var i = 0; i < num; i++) {
+      arr.push({
+        x: Math.random() * W,
+        y: Math.random() * H,
+        w: 2,
+        h: Math.random() * 30,
+        s: Math.random() * 10 + 2
+      });
+    }
+    
+    function raindrops() {
+      ctx.clearRect(0,0,W,H);
+      for(var i = 0; i < num; i++) {
+        ctx.fillStyle = 'rgba(154, 134, 161,0.2)';
+        ctx.fillRect(arr[i].x, arr[i].y, arr[i].w, arr[i].h);
+      }
+      makeItRain();
+    };
+    
+    function makeItRain() {
+      for(var i = 0; i< num; i++){ 
+       arr[i].y += arr[i].s;
+       if(arr[i].y > H){
+        arr[i].y =- arr[i].h;
+       }
+      }
+    }
+    setInterval(raindrops, 0.1);
+  };
+});
